093d8a9bebabd9ece5a403c191219e893d83d631
Psaruklily <psarukli96@gmail.com>
2021-04-09 15:32:02 +0300

    Creating stream, method subscribe

---
+++ index.js
@@ -0,0 +1,33 @@
+//LESSON 1. CREATING STRIM, SUBSCRIBE
+let stream$ = rxjs.Observable.create(function(observer) {
+    observer.next('Hello, Lili');
+
+    setTimeout(() => {
+        observer.next('After 3 seconds');
+    }, 3000);
+
+    setTimeout(() => {
+        observer.next('After 2 seconds')
+    }, 2000);
+
+    setTimeout(() => {
+        observer.next('After 5 seconds')
+    }, 5000);
+
+    observer.next('Hello, Oleh!')
+
+    observer.complete(); //форсоване завершення стріма, який і вбиває слухачів
+
+});
+
+stream$.subscribe(
+    function(data) {
+        console.log('Subscribe: ', data)
+    },
+    function(error) {
+        console.log('Error: ', error)
+    },
+    function() {
+        console.log('Completed!') // Функція, завдяки якій можна дізнатися коли завершився форсований стрім
+    }
+);
